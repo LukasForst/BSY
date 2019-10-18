1c3fea0358da75b2d2bf59fce286ab0b59f6bd17
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-18 19:32:41 +0200

    add few questions

--- encryption/encrypt_log.sh
+++ encryption/encrypt_log.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 timestamp=$(date "+%Y%m%d%H%M%S")
-for filename in logs/*.log; do
+for filename in /home/default/passworder/log*.json; do
     [ -f "$filename" ] || continue
     openssl rsautl -encrypt -inkey public_key.pem -pubin -in "$filename" -out "$filename-${timestamp}.enc"
     truncate -s 0 "$filename"
