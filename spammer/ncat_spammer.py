81061cbc6985e7c2755e2c81aa63f206c63fc2ab
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-24 18:43:39 +0200

    generate random names and ports

--- spammer/ncat_spammer.py
+++ spammer/ncat_spammer.py
@@ -1,6 +1,8 @@
 import argparse
 import subprocess
 import time
+import random
+import uuid
 
 
 def load_ips(path):
@@ -10,18 +12,23 @@ def load_ips(path):
 
 def run_ncats(ip, port, msg):
     try:
-        subprocess.run(["ncat", ip, port, "-u", msg])
+        subprocess.run(["ncat", ip, port, "-u", "<", f'\"{msg}\"'])
     except Exception as e:
         print("Exception occurred: " + str(e))
 
 
-def generate_attack_file_name(ip):
-    return "random_file.txt"
+def generate_message(ip):
+    suffix_options = [".bin", "", ".exe", ".txt", ""]
+    return f"PUSHING /files/ff/{uuid.uuid4().hex}{random.choice(suffix_options)}"
+
+
+def generate_random_port(ip):
+    return random.randint(0, 65535)
 
 
 def execute_spam(ips):
     for ip in ips:
-        run_ncats(ip, '22', generate_attack_file_name(ip))
+        run_ncats(ip, generate_random_port(ip), generate_message(ip))
 
 
 if __name__ == "__main__":
