92b540e3c4077b3c33816844d959172340578d6a
Lukas Forst <lukas.forst@blindspot.ai>
2019-10-24 18:56:10 +0200

    create logs

--- ncat_spammer/ncat_spammer.py
+++ ncat_spammer/ncat_spammer.py
@@ -4,6 +4,13 @@ import time
 import random
 import uuid
 
+LOG_FILE = "ncat_spammer.log"
+
+
+def log(to_log):
+    with open(LOG_FILE, "a", encoding="utf-8") as file:
+        file.write(to_log + "\n")
+
 
 def load_ips(path):
     with open(path, "r", encoding="utf-8") as file:
@@ -12,8 +19,11 @@ def load_ips(path):
 
 def run_ncats(ip, port, msg):
     try:
-        subprocess.run(["ncat", ip, port, "-u", "<", f'\"{msg}\"'])
+        command = ["ncat", ip, port, "-u", "<", f'\"{msg}\"']
+        log(f"Exec: {command}")
+        subprocess.run(command)
     except Exception as e:
+        log("Exception occurred: " + str(e))
         print("Exception occurred: " + str(e))
 
 
@@ -33,11 +43,16 @@ def execute_spam(ips):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--ip-addresses', dest='ip_path', type=int, default=3303,
+    parser.add_argument('-i', '--ip-addresses', dest='ip_path', type=str,
                         help="Path to file with IP addresses")
+    parser.add_argument('-l', '--logfile', dest='log_path', type=str, default="",
+                        help="Path to file where all logs should be stored")
     args = parser.parse_args()
+    LOG_FILE = f"{args.log_path.strip()}/{LOG_FILE}"
 
     ips = load_ips(args.ip_path)
+    ips_log = "\n".join(ips)
+    log(f"Spamming following addresses:{ips_log}")
 
     while True:
         execute_spam(ips)
